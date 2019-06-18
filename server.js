bbb909feb44053a1f4fd50d0dda6aed786682f01
droneal11015 <droneal11015@gmail.com>
2019-06-18 16:28:09 -0400

    incorporating day 2 files

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const session = require('express-session');
+
+const SessionStore = require('connect-session-knex')(session);
+
+const authRouter = require('./authentication/auth-router.js');
+const usersRouter = require('./users/user-router.js');
+
+const server = express();
+
+const sessionConfig = {
+  name: 'monkey',
+  secret: 'super secret string',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true
+  },
+  store: new SessionStore({
+    knex: require('./data/dbConfig.js'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+}
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig));
+
+server.use('/api/auth', authRouter);
+
+server.use('/api/users', usersRouter);
+
+server.get('/', (req, res) => {
+    res.send("Server is running")
+});
+
+  
+
+module.exports = server;
